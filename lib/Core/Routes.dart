9614113915e92719411250c6252215daf32eaa26
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-12-29 22:03:03 -0300

    #7: Criado pagina e adicionado nas rotas

--- lib/Core/Routes.dart
+++ lib/Core/Routes.dart
@@ -1,19 +1,27 @@
 import 'package:get/get.dart';
-import '../Layers/Presentation/UI/Home/HomePage.dart';
+import '../Layers/Presentation/Configuracoes/Bindings/ConfiguracoesBinding.dart';
+import '../Layers/Presentation/Configuracoes/UI/ConfiguracoesPage.dart';
+import '../Layers/Presentation/Home/UI/HomePage.dart';
 
 class JuntaPayRoutes {
   JuntaPayRoutes._();
-  
+
   static const SPLASH = '/';
   static const HOME = '/home';
+  static const CONFIGURACOES = '/configuracoes';
   static const LOGIN = '/login';
 
-  static String get initialRoute => HOME;
+  static String get initialRoute => CONFIGURACOES;
 
   static List<GetPage> get routes => [
         GetPage(
           name: HOME,
-           page: () => HomePage(),
+          page: () => HomePage(),
+        ),
+        GetPage(
+          name: CONFIGURACOES,
+          page: () => ConfiguracoesPage(),
+          binding: ConfiguracoesBinding(),
         ),
       ];
 }
